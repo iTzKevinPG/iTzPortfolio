6ebe5f106c1a288dbb86684a3520c1017db85705
iTzKevinPG <kevinusechejimenez@gmail.com>
2024-03-29 13:03:46 -0500

    :sparkles: Update file paths and remove unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpinnerComponent } from '../shared/components/spinner/spinner.component';
+import { LoaderService } from '../shared/services/loader.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, SpinnerComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'iTzPortfolio';
+export class AppComponent implements OnInit {
+	constructor(private loader: LoaderService) {}
+
+	ngOnInit() {
+		this.loader.showLoader();
+		setTimeout(() => {
+			this.loader.hideLoader();
+		}, 2000);
+	}
 }
