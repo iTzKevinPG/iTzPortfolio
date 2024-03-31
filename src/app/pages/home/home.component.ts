9f2fece28258199ff401dd13ae16307f77a613f6
iTzKevinPG <kevinusechejimenez@gmail.com>
2024-03-30 23:22:30 -0500

    Add background component to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BackgroundComponent } from '../../components/background/background.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+	selector: 'app-home',
+	standalone: true,
+	imports: [CommonModule, BackgroundComponent],
+	templateUrl: './home.component.html',
+	styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
